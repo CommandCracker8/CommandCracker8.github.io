abe85536db9eb11e37a7145162b10ec8f90ba06c
BuildTools <bunnybuffer1@gmail.com>
2021-06-03 20:21:58 +0100

    Add countdown

---
+++ countdown/js/countdown.js
@@ -0,0 +1,70 @@
+(function($) {
+	$.fn.countdown = function(options, callback) {
+
+		//custom 'this' selector
+		thisEl = $(this);
+
+		//array of custom settings
+		var settings = { 
+			'date': "June 4 2021 10:00:00 GMT+0000",
+			'format': null
+		};
+
+		//append the settings array to options
+		if(options) {
+			$.extend(settings, options);
+		}
+		
+		//main countdown function
+		function countdown_proc() {
+			
+			// eventDate = Date.parse(settings['date']) / 1000;
+			eventDate = Date.parse(settings['date']) / 1000;
+			// currentDate = Math.floor($.now() / 1000);
+			currentDate = new Date().getTime();
+			
+			if(eventDate <= currentDate) {
+				callback.call(this);
+				clearInterval(1000);
+			}
+			
+			var total = Date.parse('June 4 2021 10:00:00 GMT+0000') - Date.parse(new Date());
+			var seconds = Math.floor( (total/1000) % 60 );
+			var minutes = Math.floor( (total/1000/60) % 60 );
+			var hours = Math.floor( (total/(1000*60*60)) % 24 );
+			var days = Math.floor( total/(1000*60*60*24) );
+			
+			//conditional Ss
+			if (days == 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
+			if (hours == 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
+			if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
+			if (seconds == 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
+			
+			// //logic for the two_digits ON setting
+			if(settings['format'] == "on") {
+				days = (String(days).length >= 2) ? days : "0" + days;
+				hours = (String(hours).length >= 2) ? hours : "0" + hours;
+				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
+				seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
+			}
+			
+			//update the countdown's html values.
+			if(!isNaN(eventDate)) {
+				thisEl.find(".days").text(days);
+				thisEl.find(".hours").text(hours);
+				thisEl.find(".minutes").text(minutes);
+				thisEl.find(".seconds").text(seconds);
+			} else { 
+				alert("Invalid date. Here's an example: 12 Tuesday 2012 17:30:00");
+				clearInterval(1000); 
+			}
+		}
+		
+		//run the function
+		countdown_proc();
+		
+		//loop the function
+		interval = setInterval(countdown_proc, 1000);
+		
+	}
+}) (jQuery);
